20e8cb9b77275088481b8fba0d1efd3b56834c41
Sarnit Infotech <info@sarnitinfotech.com>
2026-05-26 15:57:11 +0530

    added Roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\AdminUserSeeder;
+use Database\Seeders\RolePermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
 
          $this->call([
         AdminUserSeeder::class,
+        RolePermissionSeeder::class,
     ]);
     }
 }
